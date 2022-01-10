a742b31721a49cb711ac92c653d2db49d3b11b8d
bulbasaurbussin <96449694+bulbasaurbussin@users.noreply.github.com>
2022-01-10 11:27:58 -0500

    Update playScreen.java
    Added set functions for playScreen

--- playScreen.java
+++ playScreen.java
@@ -4,7 +4,12 @@ import java.awt.*;
 public class playScreen {
 
     public static void main(String[] args) {
+        JFrame playScreen = new JFrame();
+        playScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        playScreen.setResizable(true);
+        playScreen.setSize(500,500);
 
+        
     }
 
 }
