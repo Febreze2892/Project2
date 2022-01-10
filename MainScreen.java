e0d1e79c2550182a3e1ef78269209977d237b3cf
BloodyPFX <ln181520@gmail.com>
2022-01-10 12:13:00 -0500

    MainScreen.Java

---
+++ MainScreen.java
@@ -0,0 +1,15 @@
+//Programmers: Luke and lukas
+//purpose: main player screen
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MainScreen {
+    public MainScreen(){
+        JFrame MainS = new JFrame("Example");
+        MainS.setSize(800,600);
+    } 
+    public static void main(String[] args) {
+        new MainScreen();
+    }
+}
