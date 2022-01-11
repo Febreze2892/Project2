8f90db5118169f2fd905da82674905274882ed11
BloodyPFX <ln181520@gmail.com>
2022-01-11 12:45:22 -0500

    Update MainScreen.java

--- MainScreen.java
+++ MainScreen.java
@@ -6,12 +6,50 @@ import javax.swing.*;
 
 public class MainScreen {
     JFrame MS;
+    JButton Store;
+    JButton Action;
+    JButton Rest;
+    JButton Inv;
+    JButton Comb;
+    JTextField Text;
+    JButton Ent;
+
     MainScreen(){
         MS = new JFrame();
         MS.setSize(800,600);
         MS.setLayout(null);
         MS.setVisible(true);
         MS.setResizable(false);
+
+
+        Store = new JButton("Shop");
+        Store.setBounds(20, 500, 132, 50);
+        MS.add(Store);
+
+        Action = new JButton("Actions");
+        Action.setBounds(172, 500, 132, 50);
+        MS.add(Action);
+
+        Rest = new JButton("Rest");
+        Rest.setBounds(324, 500, 132, 50);
+        MS.add(Rest);
+
+        Inv = new JButton("Inventory");
+        Inv.setBounds(476, 500, 132, 50);
+        MS.add(Inv);
+
+        Comb = new JButton("Fight");
+        Comb.setBounds(628, 500, 132, 50);
+        MS.add(Comb);
+
+        Text = new JTextField();
+        Text.setBounds(160, 430, 400,50);
+        MS.add(Text);
+
+        Ent = new JButton("");
+        Ent.setBounds(570, 430, 50, 50);
+        MS.add(Ent);
+    
     }
 
     
