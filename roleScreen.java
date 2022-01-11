10c9f2962fdbccb14d1b9174989ca35485381cdf
Bailey Grooms <96554047+kyuubae@users.noreply.github.com>
2022-01-11 00:24:24 -0500

    i completed the title screen :)
    a few modifications need to be made. but this is the overall theme i think should be applied to the rest of the game.

--- roleScreen.java
+++ roleScreen.java
@@ -1,31 +1,10 @@
-//Programmers: Alessa
-
-// import javax.swing.*;
-// import java.awt.*;
-
-// public class playScreen {
-
-//     public static void main(String[] args) {
-//         JFrame playScreen = new JFrame(); // creates frame
-//         playScreen.setTitle("Dungeons and Dragons");// gives GUI title
-//         playScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //allows us to close out of GUI
-//         playScreen.setResizable(false); // allows us to resize 
-//         playScreen.setSize(800,600); //sets size of GUI
-//         playScreen.setVisible(true); // allows the frame to be visible
-
-//         ImageIcon guiImage = new ImageIcon("sword.png"); // create an image for our GUI
-//         playScreen.setIconImage(guiImage.getImage()); //changes icon of our GUI
-
-
-//         playScreen.getContentPane().setBackground(Color.lightGray); //changes background color of GUI
-
 //Programmer: Bailey
-//ask mendoza about creating separate lines and double box
+//ask mendoza about creating separate lines
 
 import javax.swing.*;
 import java.awt.*;
 
-public class playScreen {
+public class roleScreen {
 
     JFrame window;
     Container con;
@@ -38,11 +17,11 @@ public class playScreen {
 
     public static void main(String[] args){
         
-        new playScreen();
+        new roleScreen();
     
     }
 
-    public playScreen(){
+    public roleScreen(){
 
         window = new JFrame();
         window.setSize(800,600);
@@ -63,7 +42,7 @@ public class playScreen {
         startButtonPanel.setBounds(300, 400, 200, 100);
         startButtonPanel.setBackground(Color.black);
 
-        startButton = new JButton("PLAY");
+        startButton = new JButton("START");
         startButton.setBackground(Color.black);
         startButton.setForeground(Color.white);
         startButton.setFont(normalFont);
@@ -79,8 +58,3 @@ public class playScreen {
 
     
 
-
-        
-//     }
-
-// }
