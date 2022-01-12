96fe2c918f6d3443d02bde18060d50788645dc4e
BloodyPFX <ln181520@gmail.com>
2022-01-12 07:50:52 -0500

    Create Store.java

---
+++ Store.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+//programmer: Lukas
+
+public class Store {
+    JFrame ST;
+    Store(){
+        ST = new JFrame();
+        ST.setSize(800,600);
+        ST.setLayout(null);
+        ST.setVisible(true);
+        ST.setResizable(false);
+    }
+}
