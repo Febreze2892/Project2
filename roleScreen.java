6758095dcadb79e3b97660f4a26e4391b467eccd
Bailey Grooms <96554047+kyuubae@users.noreply.github.com>
2022-01-12 00:18:30 -0500

    Update roleScreen.java
    im not done but i have the basics

--- roleScreen.java
+++ roleScreen.java
@@ -1,15 +1,54 @@
 //programmer: Bailey <3
+//ask if enum could be used for variables like armor protection(defense)
 
-class roleScreen{
-    private Double hp;
-    private Double stamina;
-    private int food;
-    private Double currency;
-    private int longsword;
-    private int dagger;
-    private int pocketKnife;
-    private int armor;
+import javax.swing.*;
+import java.awt.*;
 
+// class roleScreen{
+//     private Double hp;
+//     private Double stamina;
+//     private int food;
+//     private Double currency;
+//     private int longsword;
+//     private int dagger;
+//     private int pocketKnife;
+//     private int armor;
+//     private Double damage;
+// }
+
+public class roleScreen{
+    JFrame window;
+    Container con;
+    JPanel titleNamePanel, startButtonPanel;
+    JLabel titleNameLabel;
+    Font titleFont = new Font("Times New Roman", Font.PLAIN, 40);
+
+    public static void main(String []args){
+
+        new roleScreen();
+
+    }
     
+    public roleScreen(){
+        window = new JFrame();
+        window.setSize(800,600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().setBackground(Color.black); //literally pane of content, you set the background color of your content pane
+        window.setLayout(null);
+        window.setVisible(true);
+        con = window.getContentPane();
 
+        titleNamePanel = new JPanel();
+        titleNamePanel.setBounds(100, 100, 600, 150);
+        titleNamePanel.setBackground(Color.black);
+        titleNameLabel = new JLabel("Choose Your Role");
+        titleNameLabel.setForeground(Color.white);
+        titleNameLabel.setFont(titleFont);
+
+
+        titleNamePanel.add(titleNameLabel);
+        con.add(titleNamePanel);
+  
+
+    }
 }
