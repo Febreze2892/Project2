b343968cbd154e08fa73f14d649b9a403178e9da
Bailey Grooms <96554047+kyuubae@users.noreply.github.com>
2022-01-11 20:29:00 -0500

    starting role screen variables
    im setting variables and attributes n stuff

---
+++ roleScreen.java
@@ -0,0 +1,13 @@
+class roleScreen{
+    private Double hp;
+    private Double stamina;
+    private Int food;
+    private Double currency;
+    private Int longsword;
+    private Int dagger;
+    private Int pocketKnife;
+    private String armor;
+
+    
+
+}
