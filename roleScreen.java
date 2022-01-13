88df348d521dd21e9ef6f018499ef68479cc745f
Bailey Grooms <96554047+kyuubae@users.noreply.github.com>
2022-01-13 01:34:55 -0500

    create new file, added explanation
    im separating variables from roleScreen, so i gave them their own file. i also explained functions and stuff.

--- roleScreen.java
+++ roleScreen.java
@@ -1,27 +1,14 @@
-//programmer: Bailey <3
-//ask if enum could be used for variables like armor protection(defense)
+//programmer: yours truly, bailey 
 
 import javax.swing.*;
 import java.awt.*;
 
-// class roleScreen{
-//     private Double hp;
-//     private Double stamina;
-//     private int food;
-//     private Double currency;
-//     private int longsword;
-//     private int dagger;
-//     private int pocketKnife;
-//     private int armor;
-//     private Double damage;
-// }
-
 public class roleScreen{
     JFrame window;
-    Container con;
-    JPanel titleNamePanel, startButtonPanel;
+    Container con; //component that can contain other AWT components. it has many layers, with content pane being the one that holds the objects
+    JPanel titleNamePanel, startButtonPanel; //see, i have two seperate panels so thats why i named them differently
     JLabel titleNameLabel;
-    Font titleFont = new Font("Times New Roman", Font.PLAIN, 40);
+    Font titleFont = new Font("Times New Roman", Font.PLAIN, 40); //custom font yeahh poggers
 
     public static void main(String []args){
 
@@ -30,27 +17,27 @@ public class roleScreen{
     }
     
     public roleScreen(){
-        window = new JFrame();
-        window.setSize(800,600);
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        window.getContentPane().setBackground(Color.black); //literally pane of content, you set the background color of your content pane
+        window = new JFrame(); 
+        window.setSize(800,600); 
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //allows the window to be closed
+        window.getContentPane().setBackground(Color.black); //with this line, you are modifying an object with a method. according to the rule that concerns modifying objects
+        //with methods, the object will always be to the left side of the period, while your method will always be found on the right. in this case, our object is getContenPane, 
+        //which retrieves the content pane so that you may add objects to it. setBackground would be our method; although technically we are not adding an object, we are
+        //modifying the color of the pane itself.
         window.setLayout(null);
         window.setVisible(true);
-        con = window.getContentPane();
+        con = window.getContentPane(); //
 
-        titleNamePanel = new JPanel();
-        titleNamePanel.setBounds(100, 100, 600, 150);
+        titleNamePanel = new JPanel(); //i named it titleNamePanel to differentiate between other JPanels that I create 
+        titleNamePanel.setBounds(100, 100, 600, 150); //boundaries/size measured in pixels
         titleNamePanel.setBackground(Color.black);
-        titleNameLabel = new JLabel("Choose Your Role");
-        titleNameLabel.setForeground(Color.white);
-        titleNameLabel.setFont(titleFont);
+        titleNameLabel = new JLabel("Choose Your Role"); //label that is displayed within the panel
+        titleNameLabel.setForeground(Color.white); //foreground is letters
+        titleNameLabel.setFont(titleFont); //use custom font
 
 
-        titleNamePanel.add(titleNameLabel);
-        con.add(titleNamePanel);
-
-        ImageIcon sword = new ImageIcon("sword.png");
-        window.setIconImage(sword.getImage());
+        titleNamePanel.add(titleNameLabel); //appends titleNameLabel to titleNamePanel
+        con.add(titleNamePanel); //appends titleNamePanel to con
   
 
     }
